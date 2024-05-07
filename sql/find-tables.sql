5c2470e5d66ec88c29dfe1344aaf26555aa39d06
Anthony Dourish <adourish@reisystems.com>
2024-05-07 09:36:02 -0400

    include views

--- sql/find-tables.sql
+++ sql/find-tables.sql
@@ -4,13 +4,14 @@ IF OBJECT_ID('tempdb..#TableNames') IS NOT NULL
 CREATE TABLE #TableNames (TableName NVARCHAR(128))
 
 INSERT INTO #TableNames (TableName)
-VALUES ('%PostAwardConditionTracking%'), ('%GrantTermActuals%'), ('%LAL_TermActuals_P%')
+VALUES ('%PostAwardConditionTracking%'), ('%GrantTermActuals%'), ('%Term%'), ('%Condition%')
 
 CREATE TABLE #TempResult
 (
     DATABASE_NAME NVARCHAR(MAX),
     SCHEMA_NAME NVARCHAR(MAX),
-    TABLE_NAME NVARCHAR(MAX),
+    OBJECT_NAME NVARCHAR(MAX),
+    OBJECT_TYPE NVARCHAR(MAX),
     COLUMN_NAME NVARCHAR(MAX),
     DATA_TYPE NVARCHAR(MAX),
     IS_NULLABLE BIT,
@@ -27,7 +28,8 @@ BEGIN
         SELECT 
             ''?'' AS DATABASE_NAME,
             s.name AS SCHEMA_NAME,
-            t.name AS TABLE_NAME,
+            o.name AS OBJECT_NAME,
+            o.type_desc AS OBJECT_TYPE,
             c.name AS COLUMN_NAME,
             tp.name AS DATA_TYPE,
             c.is_nullable,
@@ -35,25 +37,27 @@ BEGIN
 	    CONVERT(NVARCHAR(MAX), ep.value) AS COLUMN_DESCRIPTION,
 	    CONVERT(NVARCHAR(MAX), fk.name) AS FOREIGN_KEY_NAME
         FROM 
-            sys.tables t
+            sys.objects o
         INNER JOIN 
-            sys.columns c ON t.object_id = c.object_id
+            sys.columns c ON o.object_id = c.object_id
         INNER JOIN 
             sys.types tp ON c.user_type_id = tp.user_type_id
         LEFT JOIN 
-            sys.index_columns ic ON ic.object_id = t.object_id AND ic.column_id = c.column_id
+            sys.index_columns ic ON ic.object_id = o.object_id AND ic.column_id = c.column_id
         LEFT JOIN 
             sys.indexes ind ON ic.object_id = ind.object_id AND ic.index_id = ind.index_id
         LEFT JOIN 
             sys.extended_properties ep ON ep.major_id = c.object_id AND ep.minor_id = c.column_id
         LEFT JOIN 
-            sys.foreign_key_columns fkc ON fkc.parent_object_id = t.object_id AND fkc.parent_column_id = c.column_id
+            sys.foreign_key_columns fkc ON fkc.parent_object_id = o.object_id AND fkc.parent_column_id = c.column_id
         LEFT JOIN 
             sys.foreign_keys fk ON fk.object_id = fkc.constraint_object_id
         INNER JOIN 
-            sys.schemas s ON t.schema_id = s.schema_id
+            sys.schemas s ON o.schema_id = s.schema_id
         INNER JOIN
-            #TableNames tn ON t.name LIKE tn.TableName
+            #TableNames tn ON o.name LIKE tn.TableName
+        WHERE 
+            o.type IN (''U'', ''V'') 
 END
 '
 
